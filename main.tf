1b6f2922c13be55c08452756ed9b710e875fff0e
Ivan Kuznetsov <star.zlobb@gmail.com>
2019-12-14 11:30:15 +0300

    Added SG port 80 in and all out

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
 resource "aws_instance" "instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t3.micro"
+  vpc_security_group_ids = ["${aws_security_group.web.id}"]
+
   tags = {
    Name = "web"
  }
 }
+
+resource "aws_security_group" "web" {
+  name = "Web Security Group"
+  description = "Security group"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   tags = {
+     Name = "Web"
+ }
+}
+
