25b2b2ad7a9f59a1206289ea3ad7d5a85c6329b2
Ivan Kuznetsov <star.zlobb@gmail.com>
2019-12-15 14:44:49 +0300

    create vpc

--- main.tf
+++ main.tf
@@ -1,60 +1,152 @@
-resource "aws_instance" "instance" {
+provider "aws" {
+   region = "${var.aws_region}"
+}
+
+# Query all avilable Availibility Zone
+data "aws_availability_zones" "available" {}
+
+# VPC Creation
+
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "my-new-test-vpc"
+  }
+}
+
+# Creating Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "my-test-igw"
+  }
+}
+
+# Public Route Table
+
+resource "aws_route_table" "public_route" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "my-test-public-route"
+  }
+}
+
+# Private Route Table
+
+resource "aws_default_route_table" "private_route" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+
+  tags = {
+    Name = "my-private-route-table"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  count                   = 2
+  cidr_block              = "${var.public_cidrs[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-public-subnet.${count.index + 1}"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  count             = 1
+  cidr_block        = "${var.private_cidrs[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-private-subnet.${count.index + 1}"
+  }
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_route_table.public_route.id}"
+  subnet_id      = "${aws_subnet.public_subnet.*.id[count.index]}"
+  depends_on     = ["aws_route_table.public_route", "aws_subnet.public_subnet"]
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private_subnet_assoc" {
+  count          = 1
+  route_table_id = "${aws_default_route_table.private_route.id}"
+  subnet_id      = "${aws_subnet.private_subnet.*.id[count.index]}"
+  depends_on     = ["aws_default_route_table.private_route", "aws_subnet.private_subnet"]
+}
+
+# Security Group Creation
+resource "aws_security_group" "test_sg" {
+  name   = "my-test-sg"
+  vpc_id = "${aws_vpc.main.id}"
+  tags ={
+   Name = "test_sg"
+  }
+}
+
+# Ingress Security Port 22
+resource "aws_security_group_rule" "ssh_inbound_access" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.test_sg.id}"
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "http_inbound_access" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.test_sg.id}"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# All OutBound Access
+resource "aws_security_group_rule" "all_outbound_access" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.test_sg.id}"
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+
+resource "aws_instance" "my-test-instance" {
+  count = 2
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t3.micro"
-  vpc_security_group_ids = ["${aws_security_group.web.id}"]
   key_name         = "${aws_key_pair.devops.id}"
-  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.test_sg.id}"]
+  subnet_id = "${element(aws_subnet.public_subnet.*.id,count.index)}"
 
-  provisioner "remote-exec" {
-   inline = [
-   "sudo apt update -y",
-   "sudo apt install nginx -y",
-   "sudo service nginx start"
-   ]
-  } 
-  
-  connection {
-    host     = "${aws_instance.instance.public_ip}"
-    type     = "ssh"
-    user     = "ubuntu"
-    password = ""
-    private_key = file("~/.ssh/id_rsa")
-  } 
-  
   tags = {
-   Name = "web"
- }
+    Name = "my-instance-${count.index + 1}"
+  }
 }
 
 resource "aws_key_pair" "devops" {
   key_name   = "devops"
   public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_security_group" "web" {
-  name = "Web Security Group"
-  description = "Security group"
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   tags = {
-     Name = "Web"
- }
 }
 
